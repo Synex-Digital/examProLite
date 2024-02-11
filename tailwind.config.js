170b6d3ca93198907112a66a39d7189bc0eff0cc
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-02-11 16:15:55 +0600

    login

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            screens: {
+                smalldevice: "350px",
+            },
+            colors: {
+                cGrey: "#767676",
+                cbg: "#F2F2F2",
+                cdrop: "#262626",
+                bcolor: "#D1D1D1",
+                btncolor: "#3888F9",
+                tbcolor: "#0C0C0C",
+                tgcolor: "#32B548",
+            },
+            fontFamily: {
+                rb: ["Roboto", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
